7f2185c51f46f9e74ca734f0ac071131d48f3505
Ben Peachey <ben.peachey@dealerdirect.nl>
2017-05-19 21:35:20 +0200

    Adds trait to mock functions.

---
+++ MockFunctionTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Potherca\Phpunit\MockFunction;
+
+use PHPUnit_Framework_MockObject_RuntimeException;
+
+trait MockFunctionTrait
+{
+    /** @noinspection MoreThanThreeArgumentsInspection
+     *
+     * @param string $functionName
+     * @param array $parameters
+     * @param mixed $returnValue
+     * @param array $asserts
+     *
+     * @return Builder
+     *
+     * @throws PHPUnit_Framework_MockObject_RuntimeException
+     */
+    final public function getMockFunctionBuilder($functionName, array $parameters = [], $returnValue = null, array  $asserts = [])
+    {
+        if ($this instanceof \PHPUnit\Framework\TestCase === false &&
+            $this instanceof \PHPUnit_Framework_TestCase === false
+        ) {
+            throw new PHPUnit_Framework_MockObject_RuntimeException(__TRAIT__.' can only be used by a class that extends PHPUnit_Framework_TestCase');
+        } else {
+            return new Builder($this, $functionName, $parameters, $returnValue, $asserts);
+        }
+    }
+}
+
+/*EOF*/
