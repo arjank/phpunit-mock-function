f2084d1e4f8bed83d0adea0233a4db1b05659044
Ben Peachey <ben.peachey@dealerdirect.nl>
2017-05-19 21:34:49 +0200

    Adds mock function builder.

---
+++ Builder.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Potherca\Phpunit\MockFunction;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+class Builder
+{
+    ////////////////////////////// CLASS PROPERTIES \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    /** @var string */
+    private $functionName;
+    /** @var Generator */
+    private $generator;
+    /** @var array */
+    private $parameters;
+    /** @var array */
+    private $asserts;
+    /** @var mixed */
+    private $returnValue = null;
+    /** @var TestCase */
+    private $testCase;
+
+    //////////////////////////// SETTERS AND GETTERS \\\\\\\\\\\\\\\\\\\\\\\\\\\
+    /**
+     * @return MockFunctionObject
+     */
+    final public function getMock()
+    {
+        $object = $this->generator->getMock(
+            $this->testCase,
+            $this->functionName,
+            $this->parameters,
+            $this->returnValue,
+            $this->asserts
+        );
+
+        // @TODO: Emulate $this->testCase->registerMockObject($object);
+
+        return $object;
+    }
+
+    /**
+     * @param array $asserts
+     *
+     * @return $this
+     */
+    final public function setAsserts(array $asserts = [])
+    {
+        $this->asserts = $asserts;
+
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setMockFunctionName($name)
+    {
+        $this->functionName = (string) $name;
+
+        return $this;
+    }
+
+    /**
+     * @param array $parameters
+     *
+     * @return $this
+     */
+    final public function setParameters($parameters)
+    {
+        $this->parameters = $parameters;
+
+        return $this;
+    }
+
+    /**
+     * @param $returnValue
+     *
+     * @return $this
+     */
+    final public function setReturnValue($returnValue)
+    {
+        $this->returnValue = (string) $returnValue;
+
+        return $this;
+    }
+
+    //////////////////////////////// PUBLIC API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    /**
+     * @param TestCase $testCase
+     * @param string $functionName
+     * @param array $parameters
+     * @param array $asserts
+     * @param null $returnValue
+     */
+    final public function __construct(TestCase $testCase, $functionName, array $parameters = [], $returnValue = null, array  $asserts = [])
+    {
+        $this->asserts = $asserts;
+        $this->functionName = $functionName;
+        $this->generator = new Generator();
+        $this->parameters = $parameters;
+        $this->returnValue = $returnValue;
+        $this->testCase  = $testCase;
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $assertion
+     *
+     * @return $this
+     */
+    final public function addAssert($name, $assertion)
+    {
+        $this->parameters[$name] = $assertion;
+
+        return $this;
+    }
+
+    ////////////////////////////// UTILITY METHODS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+}
+
+/*EOF*/
